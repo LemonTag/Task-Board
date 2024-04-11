25643d9b68f79efa4fe0f091f531f1e2d05636a6
Keegan Micu <LemonTag73@gmail.com>
2024-04-11 19:08:37 -0400

    add a card in the drag the card action but it doesn't work at the moment will be fixing it

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,37 +1,92 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 submitPress = document.getElementById("submit");
 // submitPress.addEventListener("click", showAlert);
 taskTitleInput = document.getElementById('taskTitle');
 taskDueInput = document.getElementById('taskDueDate');
 taskDescriptionInput = document.getElementById('taskDescription')
-
-
-
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-  //nextId != null then nextId++
+    return `task-${Date.now()}`
 }
-
+// render task list
 
 // Todo: create a function to create a task card
 // make a fake card to put up
-function createTaskCard() {
-    console.log("here");
-    let dummycard = $("#dummy-card").clone();
-    console.log(dummycard)
-    dummycard.removeClass("d-none");
-    dummycard.appendTo("#todo-cards");
+function createTaskCard(task) {
+    console.log(task);
+    const taskArray = []
+    task.forEach(taskitem => {
+        const today = dayjs()
+        const currenttask = dayjs(task.date)
+        let dateclassitem = ""
+        if (currenttask.isSame(today, "day")) {
+            dateclassitem = "worring"
+
+        } else if (currenttask.isBefore(today)) {
+            dateclassitem = "late"
+        }
+        const taskCard = `
+        <div class = "task-card ${dateclassitem}" id = ${task.id}>
+        <h3>${taskitem.title} </h3>
+        <p>${taskitem.date}</p>
+        <p>${taskitem.description}</p>
+        </div>
+    `;
+        const taskEl = $(taskCard)
+        taskArray.push(taskEl)
+    });
+
+    return taskArray
+}
+
+function renderTaskList() {
+$("#todo-cards").empty()
+$("#in-progress-cards").empty()
+$("#done-cards").empty()
+
+const taskdata = JSON.parse(localStorage.getItem("tasks")) || [];
+
+taskdata.forEach(task => {
+    const taskCard = createTaskCard([task])[0]
+    switch(task.lane){
+        case "to-do":
+            $("#to-do-cards").append(taskCard)
+            break;
+        case "in-progress":
+            $("#in-progress-cards").append(taskCard)
+            break;
+        case"done":
+            $("#done-cards").append(taskCard)
+            break;
+
+    }
+}) 
 }
 
 // Todo: create a function to render the task list and make cards draggable
-$( function draggable() {
-    $( "#dummy-card" ).draggable();
-  } );
+$(function draggable() {
+    $("#dummy-card").draggable();
+});
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
 
+    const taskTitle = $("#Task-Title").val()
+    const taskDueDate = $("#Task-Due-Date").val()
+    const taskDescription = $("#Task-Description").val()
+    const taskLane = "to-do"
+    const newtask = {
+        id: generateTaskId(),
+        title: taskTitle,
+        date: taskDueDate,
+        description: taskDescription,
+        lane: taskLane
+    }
+    taskList.push(newtask)
+    localStorage.setItem("tasks", JSON.stringify(taskList))
+    createTaskCard(taskList)
+    renderTaskList()
 }
 
 // Todo: create a function to handle deleting a task
@@ -39,63 +94,41 @@ function handleDeleteTask(event) {
 
 }
 
-
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
 }
 
-
-// function createTaskCard(event) {
-//     // have a one piece of lego/ and then break back into one pieces/and you get your peices back
-//         const stringPost = localStorage.getItem('taskinfo')
-//         const taskEntires = JSON.parse(stringPost) || [];
-//         console.log("check");
-    
-//         //you build an object with legos
-//         //this the object, has three fields of the values of the form.
-//         const taskinfo = {
-//             taskTitleEntry: taskTitleInput.value,
-//             taskDueEntry: taskDueInput.value,
-//             taskDescriptionEntry: taskDescriptionInput.value,
-//         }
-//         if (
-//             //
-//             //scan the fields to see if any of them are empty
-//             taskinfo.taskTitleEntry == '' || taskinfo.taskDueEntry == '' || taskinfo.taskDescriptionEntry == '') {
-//             console.log(username.vlaue);
-//             alert("fillin in all areas");
-//             console.log("testing");
-//             event.preventDefault();
-//         }
-    
-//         else {
-//             //take your lego into an array into a string and put it in the local storage
-//             //an Array to hold an object that gets put into this array
-//             taskEntires.push(blogEntry)
-//             //Making it into a string
-//             localStorage.setItem('blogEnties', JSON.stringify(taskEntires))
-//             console.log("nextPage");
-//         }
-    
-//     };
-
-
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 //This is where we press the task buttin
 $(document).ready(function () {
-   let taskbutton = document.getElementById("add-task")
-   console.log(taskbutton);
-   console.log("here");
-   taskbutton.addEventListener("click",function(){
-    console.log("clicked")
-    createTaskCard()
-   })
-   
+    let taskbutton = $("#add-task")
+    console.log(taskbutton);
+    console.log("here");
+    taskbutton.on("click", function (event) {
+        event.preventDefault()
+        console.log("clicked")
+        handleAddTask()
+    })
+
+    $('.task-card').draggable({
+        revert: 'invalid',
+        containment: 'document',
+      // Clone the element being dragged
+    });
+
+      // Droppable configuration
+      $('.lane').droppable({
+        accept: '.task-card',
+        drop: handleDrop
+        
+    });
+
 });
 
 function remove(button) {
     button.remove();
 }
+
 
 
